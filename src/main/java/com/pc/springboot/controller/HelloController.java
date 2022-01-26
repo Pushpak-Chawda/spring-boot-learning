6a9021af88a0352bdb3afbc1b7b8d7e3faf76315
Pushpak-Chawda <pushpaktcas@gmail.com>
2022-01-26 00:31:53 -0500

    Third Commit

--- src/main/java/com/pc/springboot/controller/HelloController.java
+++ src/main/java/com/pc/springboot/controller/HelloController.java
@@ -1,16 +1,26 @@
 package com.pc.springboot.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pc.springboot.model.UserDetails;
 
 @RestController
 public class HelloController {
 	
-	@RequestMapping(method = RequestMethod.GET,path = "/helloworld")
+	//@RequestMapping(method = RequestMethod.GET,path = "/helloworld")
+	@GetMapping("/helloworld1")
 	public String helloWorld() {
 		
 		return "Hello Pushpak";
+	}
+	
+	@GetMapping("hello-bean")
+	
+	public UserDetails getBean()
+	{
+		return new UserDetails("Pushpak", "Chawda", "Canada");
+		
 	}
 
 }
